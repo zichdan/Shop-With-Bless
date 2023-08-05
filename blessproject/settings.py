c9ee8509870e7e59f77408e6aefa123d6a7f7627
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-08-05 12:27:20 +0100

    Added urlpatterns settings for STATIC AND MEDIA files

--- blessproject/settings.py
+++ blessproject/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blessapp',
-    'blessauth',
+    # 'blessauth',
+    
+        
+    #Custom Apps
+    'core',
     
     
 ]
--- blessproject/urls.py
+++ blessproject/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 admin.site.site_header = "SHOP-WITH-BLESS"
@@ -24,6 +27,10 @@ admin.site.site_title =" SHOP-WITH-BLESS"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', include('blessapp.urls')),
+    path ('', include('core.urls')),
     path ('blessauth/', include('blessauth.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
