d23b1042a999f979ea6596125d26731494123c40
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-08-08 19:06:20 +0100

    learnt how to use default_context_processors in django settings

--- blessproject/settings.py
+++ blessproject/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'core.context_processor.default',              #adding default context processors for all arg* & kwarg* on base.html
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
