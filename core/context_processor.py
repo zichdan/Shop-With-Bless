4683f3df90f7d5d1d7aa236b19b4bc5fbfd2211c
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-08-12 09:55:55 +0100

    Price slider filter

--- core/context_processor.py
+++ core/context_processor.py
@@ -7,7 +7,7 @@ def default(request):
     categories = Category.objects.all()
     vendors = Vendor.objects.all()
 
-    # min_max_price = Product.objects.aggregate(Min("price"), Max("price"))
+    min_max_price = Product.objects.aggregate(Min("price"), Max("price"))
 
     # if request.user.is_authenticated:
     #     try:
@@ -30,5 +30,5 @@ def default(request):
     #     'wishlist':wishlist,
         'address':address,
         'vendors':vendors,
-    #     'min_max_price':min_max_price,
+        'min_max_price':min_max_price,
     }
