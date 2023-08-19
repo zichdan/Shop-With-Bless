2651e4749f24964f00056129dc1547d20bba949a
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-08-19 09:05:10 +0100

    Added useradmin url settings to project urls

--- blessproject/urls.py
+++ blessproject/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,14 +28,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', include('core.urls')),
-    path ('', include('userauths.urls')),
-    
+    path("", include("core.urls")),
+    path("user/", include("userauths.urls")),
+    path("useradmin/", include("useradmin.urls")),
+
     path("ckeditor/", include("ckeditor_uploader.urls")),
-    
 ]
 
 if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
