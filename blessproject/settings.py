8d0285a9668b999d6706e011197d9b75a66eb976
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-09-23 13:09:27 +0100

    Uncommented the db.sqlite database from gitignore

--- blessproject/settings.py
+++ blessproject/settings.py
@@ -25,16 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-45rdn!fxp2ddmv8zg4)8ux+thd@!5slw@g37+94%5*&^1e$a-r'
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = 'django-insecure-45rdn!fxp2ddmv8zg4)8ux+thd@!5slw@g37+94%5*&^1e$a-r'
+# SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.getenv('DEBUG', 'True')== "True"
+DEBUG = True
+# DEBUG = os.getenv('DEBUG', 'True')== "True"
 
 # ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1').split(',')
 
 
 
